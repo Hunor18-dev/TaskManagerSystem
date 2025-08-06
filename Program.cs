acd947118e65e34f48e0b2d071df8af33e02469e
Hunor Giyen <ghunor18@gmail.com>
2025-08-06 18:22:53 +0100

    update program cs to accept frontend requests

--- Program.cs
+++ Program.cs
@@ -11,7 +11,21 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
